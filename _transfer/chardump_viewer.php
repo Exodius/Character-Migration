4a8b7556c6f07f7e42abaa7d8e5c767c5e144e78
Helias <stefanoborzi32@gmail.com>
2015-10-17 17:55:42 +0200

    Updated chardum_viewer.php

--- _transfer/chardump_viewer.php
+++ _transfer/chardump_viewer.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 include_once("t_dbfunctions.php");
 include_once("t_functions.php");
 include_once("t_config.php");
@@ -8,9 +12,9 @@ require_once("definitions.php");
 
 if (isset($_POST["PortingType"]))
 {
-//    $o_Account = trim($_POST['Account']);
-//    $o_Password = trim($_POST['Password']);
-//    $o_URL = trim($_POST['ServerUrl']);
+    //    $o_Account = trim($_POST['Account']);
+    //    $o_Password = trim($_POST['Password']);
+    //    $o_URL = trim($_POST['ServerUrl']);
 
     $file = $_FILES['file']['tmp_name'];
     $fileopen = fopen($file, 'r');
@@ -30,9 +34,10 @@ if (isset($_POST["PortingType"]))
         $arrDump=parse_ini_string ( $buffer  );
 
         $VER = isset($arrDump["CHDMP_VER"]) ? $arrDump["CHDMP_VER"] : "<335.700";
-        if ($VER!=ADDON_VER)
-            echo "<h2>!!ATTENZIONE!!</h2> <br><br> La versione dell'addon con cui è stato estratto questo chardump è obsoleta: $VER <br> La nuova versione è la: <?=ADDON_VER?><br><br>Potresti avere problemi al termine del porting!<br>Se vuoi comunque proseguire, premi su ok ed abilita il caricamento dei chardump obsoleti nella pagina precedente!";
-
+        /*
+    if ($VER!=ADDON_VER)
+      echo "<h2>!!ATTENZIONE!!</h2> <br><br> La versione dell'addon con cui è stato estratto questo chardump è obsoleta: $VER <br> La nuova versione è la: <?=ADDON_VER?><br><br>Potresti avere problemi al termine del porting!<br>Se vuoi comunque proseguire, premi su ok ed abilita il caricamento dei chardump obsoleti nella pagina precedente!";
+*/
         $REALM_NAME = REALM_NAME;
         $DECODED_DUMP = _DECRYPT($DUMP);
         $CHAR_REALM = GetRealmID($AccountDBHost, $DBUser, $DBPassword, $AccountDB, $REALM_NAME);
@@ -56,7 +61,7 @@ if (isset($_POST["PortingType"]))
         $SIP = $row['address'];
         mysql_close($connection);
         */
-        
+
         $AchievementsCount = 0;
         $ACHMINTime = 0;
         $ACHMAXTime = 0;
@@ -69,23 +74,25 @@ if (isset($_POST["PortingType"]))
                 $ACHMAXTime = $value['D'];
             ++$AchievementsCount;
         }
-
         if (CheckGameBuild($json['ginf']['clientbuild'], $GAMEBUILD)) {
             $reason = _RT($write[50] . " " . $GAMEBUILD);
         } else if (((10 + $CharLevel > $AchievementsCount) || ($AchievementsCount > $AchievementsMinCount)) && $AchievementsCheck == 1) {
             $reason = _RT("Seems bad characters, not enought achievements!");
-        } else if (CHECKDAY($ACHMAXTime, $ACHMINTime) < $PLAYTIME) {
-            $reason = _RT("Small playtime!");
-        } else if (_CheckBlackList($AccountDBHost, $DBUser, $DBPassword, $AccountDB, $O_REALMLIST, $O_REALM, $o_URL)) {
-            $reason = _RT($write[57]);
-        }
+        }/*
+    else if (CHECKDAY($ACHMAXTime, $ACHMINTime) < $PLAYTIME) {
+      $reason = _RT("Small playtime!");
+    }
+    else if (_CheckBlackList($AccountDBHost, $DBUser, $DBPassword, $AccountDB, $O_REALMLIST, $O_REALM, $o_URL)) {
+      $reason = _RT($write[57]);
+    }
+*/
 
         $GUID = CheckCharacterGuid($AccountDBHost, $DBUser, $DBPassword, $AccountDB, $CHAR_REALM, GetCharacterGuid(_HostDBSwitch($CHAR_REALM), $DBUser, $DBPassword, _CharacterDBSwitch($CHAR_REALM)));
 
     } else if (!isset($part[1]))
         $reason = _RT($write[51]);
 
-    if (!empty($reason)) {
+        if (!empty($reason)) {
         viewerForm($reason);
     } else {
         $_SESSION['STEP2'] = "NO";
@@ -99,8 +106,7 @@ if (isset($_POST["PortingType"]))
         $GEMrow = "";
         $CURrow = "";
         $row = "";
-
-/*
+        /*
         $connection = mysql_connect(_HostDBSwitch($CHAR_REALM), $DBUser, $DBPassword);
 
         _SelectDB(_CharacterDBSwitch($CHAR_REALM), $connection);
@@ -123,12 +129,12 @@ if (isset($_POST["PortingType"]))
         `zone`          = 4395,
         `cinematic`     = 1
             WHERE `guid` = " . $GUID . ";";
-*/
-        if ($char_speccount == 2) {
-            LearnSeparateSpell(63644, $GUID, $connection);
-            LearnSeparateSpell(63645, $GUID, $connection);
-        }
 
+    if ($char_speccount == 2) {
+      LearnSeparateSpell(63644, $GUID, $connection);
+      LearnSeparateSpell(63645, $GUID, $connection);
+    }
+*/
         if ($ClassID == 6)
             $QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n " . DeathKnightTransfer($GUID);
 
@@ -143,7 +149,7 @@ if (isset($_POST["PortingType"]))
             $GlyphID4 = _GetGlyphID($value[1][0]);
             $GlyphID5 = _GetGlyphID($value[1][1]);
             $GlyphID6 = _GetGlyphID($value[1][2]);
-/*
+            /*
             $json_w['glyphs'][0][0] = _GetGlyphID($value[0][0]);
             $json_w['glyphs'][0][1] = _GetGlyphID($value[0][1]);
             $json_w['glyphs'][0][2] = _GetGlyphID($value[0][2]);
@@ -159,8 +165,8 @@ if (isset($_POST["PortingType"]))
             $achievement = $value['I'];
             $date = $value['D'];
             if (_CheckWrongOrNoAchievement($achievement))
-                echo "hai degli achievements non validi";
-                //$QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n INSERT IGNORE /* ACHIEVEMENT */ INTO `character_achievement` VALUES (" . $GUID . ", " . (int) $achievement . ", " . (int) $date . ");";
+                echo "<br>hai degli achievements non validi";
+            //$QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n INSERT IGNORE /* ACHIEVEMENT */ INTO `character_achievement` VALUES (" . $GUID . ", " . (int) $achievement . ", " . (int) $date . ");";
         }
 
         $locale = trim(strtoupper($json['ginf']['locale']));
@@ -179,8 +185,9 @@ if (isset($_POST["PortingType"]))
                 $QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n " . SonsOfHordirTransfer($GUID);
             $QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n INSERT IGNORE /* REPUTATION */ INTO `character_reputation` VALUES (" . $GUID . ", " . $faction . ", " . (int) $reputation . "," . (int) $flag . ");";
         }
-
-        foreach ($json['skills'] as $key => $value) {
+/*
+        foreach ($json['skills'] as $key => $value)
+        {
             $SkillName = mb_strtoupper($value['N'], 'UTF-8');
 
             if (_CheckRiding($SkillName, $value['C'], $connection, $GUID, $CharLevel))
@@ -196,14 +203,16 @@ if (isset($_POST["PortingType"]))
 
             if (CheckExtraSpell($SkillID))
                 LearnSeparateSpell(GetExtraSpellForSkill($SkillID, $cur, $GUID, $connection), $GUID, $connection);
-
-            $QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n INSERT IGNORE /* SKILL */ INTO `character_skills` VALUES (" . $GUID . ", " . (int) $SkillID . "," . (int) $cur . "," . (int) $max . ");";
-            
+*/
+  //          $QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n INSERT IGNORE /* SKILL */ INTO `character_skills` VALUES (" . $GUID . ", " . (int) $SkillID . "," . (int) $cur . "," . (int) $max . ");";
+/*
             if ($SpellID < 3)
                 continue;
+*/
+    //        $QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n INSERT IGNORE /* SPELL FOR SKILL */ INTO `character_spell` VALUES (" . $GUID . ", " . (int) $SpellID . ", 1, 0);";
 
-            $QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n INSERT IGNORE /* SPELL FOR SKILL */ INTO `character_spell` VALUES (" . $GUID . ", " . (int) $SpellID . ", 1, 0);";
-        }
+  //      }
+
 
         foreach ($json['spells'] as $SpellID => $value) {
             if (_isSpellValid($SpellID, $ClassID))
@@ -220,13 +229,14 @@ if (isset($_POST["PortingType"]))
             $QUERYFOREXECUTE = $QUERYFOREXECUTE . "\n INSERT IGNORE /* MOUNT OR CRITTER */ INTO `character_spell` VALUES (" . $GUID . ", " . (int) $SpellID . ", 1, 0);";
         }
 
-        mysql_close($connection);
+        //    mysql_close($connection);
+
         foreach ($json['currency'] as $key => $value) {
             $CurrencyID = $value['I'];
             $COUNT = $value['C'];
             if ($COUNT < 1)
                 continue;
-            
+
             // questi dovrebbero essere tutti i token / emblemi del player
             // l'unico filtro che fa è sugli arena / honor points che vengono aggiunti già prima
             if (_CheckCurrency($CurrencyID))
@@ -253,19 +263,35 @@ if (isset($_POST["PortingType"]))
         if (_CheckCharacterName(_HostDBSwitch($CHAR_REALM), $DBUser, $DBPassword, _CharacterDBSwitch($CHAR_REALM), $CHAR_NAME) > 0) {
             // UN AVVISO (character con lo stesso nome)
         }
-        
-        
-        print_r($json);
-        
+
+//        print_r($json);
+
         // il risultato sarà stampato qui
         // le chiavi da estrarre sono:
-        
-        
+
+
     }
 } else {
     viewerForm();
+    echo '
+  <h1>Visualizzatore Chardump</h1>
+  <form action="" method="POST" enctype="multipart/form-data">
+<div>
+  <b>Tipologia di porting: </b><br><input name="PortingType" value="0" required type="radio">
+  <b><span class="porting-type">Free</span></b>
+  <b><input name="PortingType" value="1" required type="radio"> <b><span class="porting-type">Basic</span></b>
+  <b><input name="PortingType" value="2" required type="radio"> <b><span class="porting-type">Full</span></b>
+</div>
+
+<div class="MythInput">
+    <style =="" "font-size:14px"="">File selezionato:</style>
+      <input name="file" id="file" accept=".lua" type="file">
+      <input name="load" value="Visualizza chardump" type="submit">
+</div>
+</form>
+';
 }
 
 function viewerForm($err="") {
-    echo "<br><br> $err <br><br>";
+    echo "$err";
 }
