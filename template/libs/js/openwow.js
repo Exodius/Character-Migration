ba2a443a82caad6f0482336cf1e3541ccc0638dc
Yehonal <hw.2@hotmail.it>
2016-01-06 12:58:58 +0100

    rewrited loadItem function improving performances and reliability

--- template/libs/js/openwow.js
+++ template/libs/js/openwow.js
@@ -1,40 +1,38 @@
 var AzthOpenwow = function () {
-
-    var _loaded = false;
-
-    var _bak = $oWoWTooltip.init;
-
-    $oWoWTooltip.init = function () {
-        _bak();
-
-        _loaded = true;
-    };
-
     this.loadItem = function (currentType, currentId, currentLocale, currentParams, currentExpansion, callback) {
-        var that = this;
+        // do everything after document has been fully load to avoid page freezing
+        $(document).ready(function () {
+            var t;
+            var reqSent = false;
+            function _loadItem () {
+                // if this function exists, than the $oWoW system has been loaded , otherwise wait for it
+                if (!$oWoW.getDomainFromExpansion) {
+                    setTimeout(function () {
+                        _loadItem();
+                    }, 500);
 
-        if (_loaded === false) {
-            setTimeout(function () {
-                that.loadItem(currentType, currentId, currentLocale, currentParams, currentExpansion, callback);
-            }, 100);
+                    return;
+                } else {
 
-            return;
-        }
+                    // send request just 1 time
+                    if (!reqSent) {
+                        $oWoWTooltip.displayTooltip(currentType, currentId, currentLocale, currentParams, currentExpansion);
+                        reqSent = true;
+                    }
 
-        var t;
-        function _loadItem () {
-            $oWoWTooltip.displayTooltip(currentType, currentId, currentLocale, currentParams, currentExpansion);
-
-            if (LOOKUPS[currentType][0][currentExpansion][currentId]["icon"]) {
-                callback(LOOKUPS[currentType][0][currentExpansion][currentId]);
-                clearTimeout(t);
-                return;
-            } else {
-                t = setTimeout(_loadItem, 100);
+                    // wait that info has been received
+                    if (LOOKUPS[currentType][0][currentExpansion][currentId]["icon"]) {
+                        callback(LOOKUPS[currentType][0][currentExpansion][currentId]);
+                        clearTimeout(t);
+                        return;
+                    } else {
+                        t = setTimeout(_loadItem, 500);
+                    }
+                }
             }
-        }
 
-        _loadItem(callback);
+            _loadItem();
+        });
     };
 
     this.loadIcon = function (imgTarget, currentType, currentId, currentLocale, currentParams, currentExpansion) {
